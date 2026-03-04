bbc28bbf8fea663b295343e54c2b85704059691b
Chandragupt <chandraguptacharya02@gmail.com>
2026-03-04 16:49:47 +0530

    FindAllDisappearedNumberInAnArray

--- StoneyCodes/Arrays/FindAllDisappearedNumberInAnArray(LC448).py
+++ StoneyCodes/Arrays/FindAllDisappearedNumberInAnArray(LC448).py
@@ -7,3 +7,17 @@ class Solution:
             if i not in numSet:
                 result.append(i)
         return result
+    
+# Using HashMap
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        res=[]
+        n=len(nums)
+        freqMap={i:0 for i in range(1,n+1)}
+        for num in nums:
+            freqMap[num]+=1
+        for k,v in freqMap.items():
+            if v==0:
+                res.append(k)
+        return res
